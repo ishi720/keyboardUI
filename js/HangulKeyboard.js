dbd62a0b7c65ddacb2ed7842ccd3d5a2d5901b11
ishi720 <ishizuka720@gmail.com>
2025-09-18 22:38:57 +0900

    プライベートメソッド反映の漏れ修正

--- js/HangulKeyboard.js
+++ js/HangulKeyboard.js
@@ -66,7 +66,7 @@ export default class HungulKeyboard extends KeyboardBase {
     /**
      * 分解済みテキストをリセット
      */
-    decomposeText() {
+    #decomposeText() {
         this.currentText = this.#decomposeHangul(this.originalText);
     }
 
